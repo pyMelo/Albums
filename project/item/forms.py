9ebd9e3c4f83dbdf84cea637499085b761d4952a
Melo <83662708+pyMelo@users.noreply.github.com>
2024-03-18 17:06:14 +0100

    Update forms.py

--- project/item/forms.py
+++ project/item/forms.py
@@ -22,23 +22,6 @@ class NewItemForm(forms.ModelForm):
                 'class': INPUT_CLASSES
             })
         }
-
-class EditItemForm(forms.ModelForm):
-    class Meta:
-        model = Item
-        fields = ('name', 'description', 'image')
-        widgets = {
-            'name': forms.TextInput(attrs={
-                'class': INPUT_CLASSES
-            }),
-            'description': forms.Textarea(attrs={
-                'class': INPUT_CLASSES
-            }),
-            'image': forms.FileInput(attrs={
-                'class': INPUT_CLASSES
-            })
-        }
-    
     
 from django import forms
 from .models import Rating
